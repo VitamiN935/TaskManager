e1c92e3d3a5ffa3d5298346592c51f81c57660c7
Maksim_Smetanin <maks.s923@mail.ru>
2019-10-23 21:33:14 +0500

    create and initial start views, create correcrted router

---
+++ src/views/CreateTask.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h2 class="center">Создание задачи</h2>
+    <hr />
+    <form class="col s12 m12 form">
+      <div class="row">
+        <div class="input-field col s10 m8">
+          <i class="material-icons prefix">title</i>
+          <input class="validate" type="text" id="first_name" placeholder="Placeholder" />
+          <label for="first_name">Название задачи</label>
+        </div>
+        <div class="input-field col s10 m8 textarea">
+          <i class="material-icons prefix">description</i>
+          <textarea
+            class="materialize-textarea"
+            type="text"
+            id="textarea1"
+            placeholder="Placeholder"
+          ></textarea>
+          <label for="textarea1">Описание</label>
+        </div>
+        <div class="input-field col s8 m6">
+          <div class="chips"></div>
+        </div>
+        <div class="input-field col s6 m4">
+          <input class="datepicker" type="text" />
+        </div>
+        <div class="input-field col s10 m8">
+          <button class="btn right waves-effect">Создать задачу</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
---
+++ src/views/TasksList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container m12">
+    <h2 class="center">Список задач</h2>
+    <hr />
+    <div class="row">
+      <table class="centered">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Название</th>
+            <th>Описание</th>
+            <th>Дата</th>
+            <th>Тэги</th>
+            <th>Статус</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>title</td>
+            <td>descriprion</td>
+            <td>12.32.43</td>
+            <td>vue</td>
+            <td>
+              <span class="white-text badge green">active</span>
+            </td>
+            <td>
+              <button class="btn btn-sm waves-effect">
+                <i class="tiny material-icons">input</i>
+              </button>
+            </td>
+            <td>
+              <button class="btn btn-sm red waves-effect">
+                <i class="tiny material-icons">remove_circle</i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
